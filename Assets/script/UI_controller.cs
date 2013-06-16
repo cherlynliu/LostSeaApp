522aca3b226e096056f861fa7222f70232cb3dfa
cherlyn liu <cl_liu@hotmail.com>
2013-06-16 23:33:52 +0800

    匯出，修改成適合PC直式視窗(自己調整視窗)

--- Assets/script/UI_controller.cs
+++ Assets/script/UI_controller.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class UI_controller : MonoBehaviour {
     //健康、體力、天數的數值
-    public static int iBarG_width = 300, iBarR_width = 300, iDayPast = 0;   
+    public static int iBarG_width = 300, iBarR_width = 300, iDayPast = 0;
+    int max = 300;
     //里程數、體力耗損計算-->從player_controller裡面累加(移動時)、健康耗損-->吃到魚扣一點點
     public static float mileage = 0.0f, strengthDecay = 0.0f, healthDecay = 0.0f;
     public GUISkin GUISkin_t;
@@ -16,13 +17,13 @@ public class UI_controller : MonoBehaviour {
     // Update is called once per frame
 	void Update () {
         iDayPast = DayCounter(mileage); //計算天數(依移動里程增加)
-        iBarR_width = 300 - StrengthBarCounter(strengthDecay);  //體力耗損(前進後退扣較多、左右移動較少)
-        iBarG_width = 300 - (int)healthDecay;  //健康耗損
-        if (iBarR_width > 300) iBarR_width = 300;
-        if (iBarG_width > 300) iBarG_width = 300;
+        iBarR_width = max - StrengthBarCounter(strengthDecay);  //體力耗損(前進後退扣較多、左右移動較少)
+        iBarG_width = max - (int)healthDecay;  //健康耗損
+        if (iBarR_width > max) iBarR_width = max;
+        if (iBarG_width > max) iBarG_width = max;
         //介面元件更動
-        GameObject.Find("GUI green bar").GetComponent<GUITexture>().pixelInset = new Rect(150, -40, iBarG_width, 20);
-        GameObject.Find("GUI red bar").GetComponent<GUITexture>().pixelInset = new Rect(150, -60, iBarR_width, 20);
+        GameObject.Find("GUI green bar").GetComponent<GUITexture>().pixelInset = new Rect(120, -40, iBarG_width, 20);
+        GameObject.Find("GUI red bar").GetComponent<GUITexture>().pixelInset = new Rect(120, -60, iBarR_width, 20);
         GameObject.Find("GUI Text_day_num").GetComponent<GUIText>().text = string.Format("{0:D4}",iDayPast).ToString();
 	}
     int DayCounter(float value) {
